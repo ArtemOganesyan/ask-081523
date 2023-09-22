f232e8281d78f67c7d4c7a5dd945aa80cb71b0bd
AnnaSh <testjoy2015@gmail.com>
2023-09-21 20:04:07 -0700

    Updated AnnaSh TC Quiz-Updated AT

--- src/test/java/definitions/SharoikoStepDefs.java
+++ src/test/java/definitions/SharoikoStepDefs.java
@@ -9,13 +9,13 @@ import static org.assertj.core.api.Assertions.*;
 import static support.TestContext.getDriver;
 import org.openqa.selenium.WebElement;
 
-public class SharoikoStepDsfs {
+public class SharoikoStepDefs {
     @Then("AS validate the date with xpath {string}")
     public void iValidateTheLocalTimeWithXpath(String xpath) {
         WebElement we = getDriver().findElement(By.xpath(xpath));
-        String updated_field = we.getText().substring(0, we.getText().length() - 3);
+        String updated_field = we.getText().substring(0, we.getText().length() - 4);
         System.out.println(updated_field);
-        String date_format = "MM/dd/yyyy HH:mm";
+        String date_format = "MM/dd/yyyy HH:m";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(date_format);
         System.out.println(LocalDateTime.now().format(formatter));
         String expected = LocalDateTime.now().format(formatter).toString();
