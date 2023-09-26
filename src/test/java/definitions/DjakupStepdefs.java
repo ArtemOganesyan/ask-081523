79a4116852ab3ceb32b8ee4df6f2bca0db616444
Meerim Djakupbekova <meerim1218@gmail.com>
2023-09-25 23:29:47 -0400

    Scenario added

---
+++ src/test/java/definitions/DjakupStepdefs.java
@@ -0,0 +1,19 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class DjakupStepdefs {
+    @Given("DM open url {string}")
+    public void dmOpenUrl(String arg0) {
+
+    }
+
+    @When("DM type {string} into element with xpath {string}")
+    public void dmTypeIntoElementWithXpath(String arg0, String arg1) {
+
+    }
+
+    @And("DM click on element with xpath {string}")
+    public void dmClickOnElementWithXpath(String arg0) {
+    }
+}
